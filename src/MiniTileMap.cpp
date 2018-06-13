e6bfac8a6a71021e7a2aa0c31092e9a037921bb4
Luiza Amanajas <luizamanajas@hotmail.com>
2018-06-13 10:58:14 -0300

    Minimap scroll acompanhando a camera

--- src/MiniTileMap.cpp
+++ src/MiniTileMap.cpp
@@ -1,5 +1,6 @@
 #include "MiniTileMap.h"
 #include <algorithm>
+#include "Camera.h"
 
 MiniTileMap::MiniTileMap(GameObject &associated, TileSet *tileSet,
                          TileMap *tileMap, Alan *alan)
@@ -15,15 +16,16 @@ void MiniTileMap::Render(Common::Layer layer) const {
     int y = associated.box.y;
     int valPos;
     Vec2 alanPos = alan->GetGridPosition();
+    int yMin = Camera::pos.y/alan->GetGridSizeHeight();
 
-    for (int posY = 0; posY < tileMap->GetHeight(); posY++) {
+    for (int posY = yMin; posY < tileMap->GetHeight(); posY++) {
         for (int posX = 0; posX < tileMap->GetWidth(); posX++) {
             // Coloca a marmota no minimapa
             valPos = tileMap->At(posX, posY);
             if (alanPos.x == posX && alanPos.y == posY) {
                 valPos = 5;
                 // Faz a borda o minimapa
-            } else if (posY > alan->GetMaxPosition()) {
+            } else if (posY > alan->GetMaxPosition() + 7) {
                 valPos = 1;
             }
             tileSet->RenderTile(valPos, x, y);
