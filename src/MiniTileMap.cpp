718cd0e6ab4fc96431f9add2d5c6c16d17b61058
Luiza Amanajas <luizamanajas@hotmail.com>
2018-06-23 19:05:48 -0300

    MiniTileMap aparecendo só itens
    MiniAlan.y centralizado no minimap

--- src/MiniTileMap.cpp
+++ src/MiniTileMap.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include "Alan.h"
 #include "Camera.h"
+#include "Common.h"
 #include "Game.h"
 
 MiniTileMap::MiniTileMap(GameObject &associated, TileSet *tileSet,
@@ -16,24 +17,33 @@ MiniTileMap::~MiniTileMap() {}
 void MiniTileMap::Render(Common::Layer layer) const {
     if (!alan.lock()) return;
 
+    int yMin;
     int x = associated.box.x;
     int y = associated.box.y;
     int valPos;
 
     Vec2 alanPos = alan.lock()->GetGridPosition();
-    int yMin =
-        Camera::pos.y / Game::GetInstance()->GetCurrentState().GetGridSize();
+
+    yMin = alanPos.y - 5;
+
+    // Camera::pos.y / Game::GetInstance()->GetCurrentState().GetGridSize();
 
     for (int posY = yMin; posY < tileMap->GetHeight(); posY++) {
         for (int posX = 0; posX < tileMap->GetWidth(); posX++) {
             // Coloca a marmota no minimapa
-            valPos = tileMap->At(posX, posY);
-            if (alanPos.x == posX && alanPos.y == posY) {
+            // valPos = tileMap->At(posX, posY, TileMap::Layers::BLOCOS);
+            if (posY < 0) {
+                valPos = 1;
+            } else if (alanPos.x == posX && alanPos.y == posY) {
                 valPos = 5;
                 // Faz a borda o minimapa
+            } else if (tileMap->At(posX, posY, TileMap::Layers::ITENS)) {
+                valPos = 4;
             } else if (posY >
                        alan.lock()->GetComponent<Alan *>()->GetMaxPosition() +
                            7) {
+                valPos = 1;
+            } else {
                 valPos = 1;
             }
             tileSet->RenderTile(valPos, x, y);
