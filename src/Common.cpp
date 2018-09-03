2cf19d409396f69ffe6e612b189e8bcfb29a7a04
Diego Resende <wdresende@gmail.com>
2018-09-03 07:46:25 -0300

    fixing name of functions

--- src/Common.cpp
+++ src/Common.cpp
@@ -1,6 +1,6 @@
 #include "Common.h"
 
-char *Common::file_read(const char *file_name)
+char *Common::fileRead(const char *file_name)
 {
     SDL_RWops *rw = SDL_RWFromFile(file_name, "rb");
     if (rw == nullptr) return nullptr;
@@ -27,7 +27,7 @@ char *Common::file_read(const char *file_name)
 
 void Common::readJson(json &j, const std::string &file)
 {
-    char *data = Common::file_read(file.c_str());
+    char *data = Common::fileRead(file.c_str());
     if (data == nullptr) exit(0);
 
     j = json::parse(data, data + strlen(data));
