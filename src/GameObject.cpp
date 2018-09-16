d891aef089c246581386b81c17e799cc60bcc475
Raul Victor <rvictors@hotmail.com>
2018-09-16 19:12:59 -0300

    [modulo 2]#1 GameObject.cpp

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,55 +1,64 @@
 #include "GameObject.h"
 #include "Sound.h"
 
-bool GameObject::CanEnd() const { return true; }
+bool GameObject::canEnd() const { return true; }
 
-GameObject::~GameObject() {
+GameObject::~GameObject()
+{
     for (Component* component : components) delete (component);
 
     components.clear();
     tmp = box;
 }
 
-void GameObject::Update(float dt) {
+void GameObject::update(float dt)
+{
     static float sum;
     sum += dt;
-    for (Component* component : components) component->Update(dt);
-    if (move) {
+    for (Component* component : components) component->update(dt);
+    if (move)
+    {
         box.pos = tmp.pos + Vec2(0, 1) * sin(sum) * 30;
     }
 }
 
-void GameObject::RhythmUpdate() {
-    for (Component* component : components) component->RhythmUpdate();
+void GameObject::rhythmUpdate()
+{
+    for (Component* component : components) component->rhythmUpdate();
 }
 
-void GameObject::RhythmReset() {
-    for (Component* component : components) component->RhythmReset();
+void GameObject::rhythmReset()
+{
+    for (Component* component : components) component->rhythmReset();
 }
 
-void GameObject::RenderOrder(Common::Layer layer) const {
+void GameObject::renderOrder(Common::Layer layer) const
+{
     if (!(this->layer & layer)) return;
 
-    for (Component* component : components) component->Render(layer);
+    for (Component* component : components) component->render(layer);
 }
 
-void GameObject::RemoveComponent(Component* cpt) {
+void GameObject::removeComponent(Component* cpt)
+{
     components.erase(std::remove(components.begin(), components.end(), cpt),
-                     components.end());
+    components.end());
 }
 
-void GameObject::AddComponent(Component* cpt) { components.emplace_back(cpt); }
+void GameObject::addComponent(Component* cpt) { components.emplace_back(cpt); }
 
-void GameObject::CopyPosition(const GameObject& go) {
+void GameObject::copyPosition(const GameObject& go)
+{
     box = go.box;
-    worldReference = go.worldReference;
-    fromPlayer = go.fromPlayer;
-    angleDeg = go.angleDeg;
+    world_reference = go.world_reference;
+    from_player = go.from_player;
+    angle_deg = go.angle_deg;
 }
 
-void GameObject::Start() {
+void GameObject::start()
+{
     if (started) return;
-    for (auto c : components) c->Start();
+    for (auto c : components) c->start();
 
     started = true;
 }
