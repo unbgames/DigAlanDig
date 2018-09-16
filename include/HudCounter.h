9fe67932292f226c6dda7cef44da2fa955d36739
Raul Victor <rvictors@hotmail.com>
2018-09-16 19:23:50 -0300

    [modulo 2]#1 HudCounter.h

--- include/HudCounter.h
+++ include/HudCounter.h
@@ -3,21 +3,22 @@
 #include "Component.h"
 #include "Sprite.h"
 
-class HudCounter : public Component {
-  public:
-    HudCounter(GameObject& associated, const std::string& bgFile,
-               const std::string& itemFile);
-    ~HudCounter();
+class HudCounter : public Component
+{
+    public:
+        HudCounter(GameObject& associated, const std::string& bgFile,
+                   const std::string& itemFile);
+        ~HudCounter();
 
-    void Update(float dt) {}
+        void Update(float dt) {}
 
-    void RhythmUpdate() {}
-    void Render(Common::Layer layer) const;
+        void RhythmUpdate() {}
+        void Render(Common::Layer layer) const;
 
-    Sprite *bg, *counter;
-    Rect boxbg;
-    Vec2 offset1;
-    Vec2 offset2;
+        Sprite *bg, *counter;
+        Rect boxbg;
+        Vec2 offset1;
+        Vec2 offset2;
 };
 
-#endif  // HUDCOUNTER_H
+#endif
