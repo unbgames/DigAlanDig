52756dfc2c14aa0f72a945cb884aba73f1980328
Raul Victor <rvictors@hotmail.com>
2018-09-16 19:24:30 -0300

    [modulo 2]#1 Music.h

--- include/Music.h
+++ include/Music.h
@@ -8,22 +8,23 @@
 #include <string>
 #include "Resources.h"
 
-class Music {
-  public:
-    Music() : music(nullptr) {}
-    explicit Music(const std::string &file) : music(nullptr) { Open(file); }
-    ~Music() {}
+class Music
+{
+    public:
+        Music() : music(nullptr) {}
+        explicit Music(const std::string &file) : music(nullptr) { Open(file); }
+        ~Music() {}
 
-    void Play(int times = -1) const;
-    void Stop(int msToStop = 1500) const { Mix_FadeOutMusic(msToStop); }
-    void Open(const std::string &file) { music = Resources::GetMusic(file); }
+        void Play(int times = -1) const;
+        void Stop(int msToStop = 1500) const { Mix_FadeOutMusic(msToStop); }
+        void Open(const std::string &file) { music = Resources::GetMusic(file); }
 
-    bool IsOpen() const { return (bool)music; }
+        bool IsOpen() const { return (bool)music; }
 
-    Mix_Music *GetMixMusic() { return music.get(); }
+        Mix_Music *GetMixMusic() { return music.get(); }
 
-  private:
-    std::shared_ptr<Mix_Music> music = nullptr;
+    private:
+        std::shared_ptr<Mix_Music> music = nullptr;
 };
 
 #endif
