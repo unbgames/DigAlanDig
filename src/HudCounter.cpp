a56f1949c6f83304b694e84c18e0e843cfacc1d9
Raul Victor <rvictors@hotmail.com>
2018-09-16 19:15:05 -0300

    [modulo2]#1 HudCounter.cpp

--- src/HudCounter.cpp
+++ src/HudCounter.cpp
@@ -2,24 +2,25 @@
 #include "InputManager.h"
 
 HudCounter::HudCounter(GameObject& associated, const std::string& bgFile,
-                       const std::string& itemFile)
-    : Component(associated) {
-    bg = new Sprite(associated, bgFile);
-    boxbg = associated.box;
+                       const std::string& itemFile) : Component(associated)
+    {
+        bg = new Sprite(associated, bgFile);
+        boxbg = associated.box;
 
-    counter = new Sprite(associated, itemFile, 1);
+        counter = new Sprite(associated, itemFile, 1);
 
-    offset1 = (boxbg.size - associated.box.size) / 2.0;
+        offset1 = (boxbg.size - associated.box.size) / 2.0;
 
-    // offset2 = {(internalBox.w - associated.box.w) / 2, 0};
-}
+    }
 
-HudCounter::~HudCounter() {
+HudCounter::~HudCounter()
+{
     delete bg;
     delete counter;
 }
 
-void HudCounter::Render(Common::Layer layer) const {
+void HudCounter::Render(Common::Layer layer) const
+{
     associated.box = boxbg;
     bg->Render(layer);
 
