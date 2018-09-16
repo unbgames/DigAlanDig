4c80f20abd79437e9b9256393abad6070cd1ff08
Raul Victor <rvictors@hotmail.com>
2018-09-16 19:16:57 -0300

    [modulo 2]#1 Music.cpp

--- src/Music.cpp
+++ src/Music.cpp
@@ -1,8 +1,12 @@
 #include "Music.h"
 
-void Music::Play(int times) const {
-    if (music) {
+void Music::Play(int times) const
+{
+    if (music)
+    {
         if (Mix_PlayMusic(music.get(), times) == -1)
+        {
             std::cerr << "Mix_PlayMusic: " << Mix_GetError() << std::endl;
+        }
     }
 }
